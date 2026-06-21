232a18f617011f4ec02ea1e7eba6df5fcff289a7
Jiaaaa <jia18573785021@gmail.com>
2026-06-21 15:18:19 +0800

    Add UE5 material lab project

---
+++ Source/UE5_Material_Lab/UE5_Material_Lab.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UE5_Material_Lab.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UE5_Material_Lab, "UE5_Material_Lab" );
